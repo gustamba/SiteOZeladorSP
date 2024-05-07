32d19eded59012e2109fa5e29d1c98f16198869a
gustamba <gustamba7813@gmail.com>
2024-05-07 18:42:23 -0300

    Responsividade

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectToWhatsapp() {
+    window.location.href = "https://wa.me/5511992705129?text=Quero+saber+mais+sobre+o+treinamento+para+porteiros+e+controladores+de+acesso%21"
+}
+
+function redirectToTreinamento(){
+    window.location.href = "#TreinamentoPorteiros"
+}
+
+function redirectToContact(){
+    window.location.href = "#contato"
+}
